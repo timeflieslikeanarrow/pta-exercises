7686efec1b52fa1cd089a132dfc30190576a2d40
Jian Shi <timeflieslikeanarrow001@gmail.com>
2021-03-14 23:49:47 -0400

    made the exe run

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import qualified DS2021Spring.IsomorphicTree as IsoTree
 import qualified DS2021Spring.MaxSubseqSum as MaxSum
+import qualified DS2021Spring.MaxSubseqSumWithEnds as MaxSumEnds
 
 main :: IO ()
-main = MaxSum.main
+main = MaxSumEnds.main
